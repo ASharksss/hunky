82b2d1a9431440c7926f91648cb7ae868ac58095
TSharksss <tsharksss@gmail.com>
2023-01-09 14:32:10 +0300

    bug fix

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,38 +7,37 @@ import { requestJobList } from '../actions/user';
 export const Home = () => {
   const dispatch = useDispatch()
   const jobs = useSelector(state => state.user.jobs)
-  const [job, setJob] = useState(jobs)
   const [search, setSearch] = useState('')
 
   useEffect(() => {
     dispatch(requestJobList())
   }, [])
 
-  const filteredJobs = job.filter(item => {
-    return item.name.toLowerCase().includes(search)
-  })
+  let filteredJobs = jobs.filter(item => item.name.toLowerCase().includes(search))
 
-  if (jobs) {
-    return (
-      <div className='home'>
-        <div className="container">
-          <div className="home__wrapper">
-            <div className="btn_wrapper">
-              <input value={search} onChange={e => setSearch(e.target.value.toLowerCase())}
-                type="text" placeholder='Поиск' className='home__search' />
-              {filteredJobs.map(item => {
-                return (
-                  <NavLink to={"/product/" + item.id} className='home__btn_link'>
-                    <button className='home__btn'>
-                      {item.name}
-                    </button>
-                  </NavLink>
-                )
-              })}
-            </div>
+  useEffect(() => {
+    filteredJobs = jobs
+  }, [jobs])
+
+  return (
+    <div className='home'>
+      <div className="container">
+        <div className="home__wrapper">
+          <div className="btn_wrapper">
+            <input value={search} onChange={e => setSearch(e.target.value.toLowerCase())}
+              type="text" placeholder='Поиск' className='home__search' />
+            {filteredJobs.map(item => {
+              return (
+                <NavLink id={item.id} to={"/product/" + item.id} className='home__btn_link'>
+                  <button className='home__btn'>
+                    {item.name}
+                  </button>
+                </NavLink>
+              )
+            })}
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 };
--- src/components/HomeDetail.jsx
+++ src/components/HomeDetail.jsx
@@ -16,7 +16,7 @@ export const HomeDetail = () => {
 		dispatch(requestGetJob(id))
 	}, [])
 
-	const handleSubmit = async() => {
+	const handleSubmit = async () => {
 		const data = {
 			'name': job.name,
 			'type': detail,
@@ -25,41 +25,42 @@ export const HomeDetail = () => {
 		}
 		await dispatch(requestAddJob(data))
 	}
+	if (job.length !== 0) {
+		return (
+			<div className='home_detail'>
+				<div className="container">
+					<div className="detail__wrapper">
+						<form className='detail_form' onSubmit={e => {
+							e.preventDefault()
+							handleSubmit()
+						}}>
+							<label>{job.name}</label>
+							<select onChange={e => setDetail(e.target.value)} value={detail}
+								className="home_detail__select">
+								<option defaultValue hidden>Выберите вид...</option>
+								{job.info.map(item => {
+									return (
+										<option key={item} value={item}>{item}</option>
+									)
+								})}
+							</select>
+							<input type='number' value={count} onChange={e => setCount(e.target.value)}
+								placeholder='Количество сделано' />
+							<input type='number' value={defect} onChange={e => setDefect(e.target.value)}
+								placeholder='Количество брак' />
 
-	return (
-		<div className='home_detail'>
-			<div className="container">
-				<div className="detail__wrapper">
-					<form className='detail_form' onSubmit={e => {
-						e.preventDefault()
-						handleSubmit()
-					}}>
-						<label>{job.name}</label>
-						<select onChange={e => setDetail(e.target.value)} value={detail}
-							className="home_detail__select">
-							<option defaultValue hidden>Выберите вид...</option>
-							{job.info.map(item => {
-								return (
-									<option key={item} value={item}>{item}</option>
-								)
-							})}
-						</select>
-						<input type='number' value={count} onChange={e => setCount(e.target.value)}
-							placeholder='Количество сделано' />
-						<input type='number' value={defect} onChange={e => setDefect(e.target.value)}
-							placeholder='Количество брак' />
+							<div className="detail_btns">
+								<button type='submit' className='detail_btn'>
+									Сохранить
+								</button>
+							</div>
 
-						<div className="detail_btns">
-							<button type='submit' className='detail_btn'>
-								Сохранить
-							</button>
-						</div>
-
-						{error ? <p className="error">{error}</p> : ''}
-					</form>
+							{error ? <p className="error">{error}</p> : ''}
+						</form>
+					</div>
 				</div>
 			</div>
-		</div>
-	);
+		);
+	}
 };
 
